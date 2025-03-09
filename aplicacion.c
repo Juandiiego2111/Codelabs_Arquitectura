5246d17c02038eb94df9d0824a67b27ddab1e172
Juandiiego2111 <juan.rodriguez16@uceva.edu.co>
2025-03-09 18:42:58 +0000

    Mis codelabs

---
+++ aplicacion.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+printf ("\nHola mundo!\n");
+return 0;
+
+}  
---
+++ ejemplo-1.s/ejemplo-1.s
@@ -0,0 +1,13 @@
+.global _start
+
+.text
+
+_start:
+
+       mov $0x3f, %al
+       mov $0xF3, %ah
+
+       # Salir al sistema
+       mov $60, %rax
+       xor %rdi, %rdi
+       syscall
